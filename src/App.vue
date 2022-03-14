5d7e3042c27bcf4da530c20b2782faaf5227cb94
unknown <you@example.com>
2022-03-14 19:47:37 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Actions')
+
+</script>
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
